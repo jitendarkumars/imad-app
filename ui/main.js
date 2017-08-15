792b1647ef356819c278d57dce1f55603ac9c6d5
jitendarkumars <jitendarkumar404@gmail.com>
2017-08-15 18:08:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+
+var img= document.getElementById("jeet");
+var marginLeft=0;
+
+function moveRight()
+{
+    marginLeft= marginLeft+1;
+    img.style.marginLeft= marginLeft +"px";
+}
+
+img.onclick = function()
+{
+    var interval = setInterval(moveRight,10);
+}
